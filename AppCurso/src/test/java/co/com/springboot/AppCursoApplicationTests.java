946090c87172d20e8b64c6f4ad748ed2b8aba5d2
JUAN <JUAN@192.168.1.57>
2020-05-23 18:19:30 -0500

    Comit inicial con eclipse

---
+++ AppCurso/src/test/java/co/com/springboot/AppCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
