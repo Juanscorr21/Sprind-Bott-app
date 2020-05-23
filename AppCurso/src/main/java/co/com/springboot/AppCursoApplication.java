f6df33c403ddc6acbda1db78399747722483b065
JUAN <JUAN@192.168.1.57>
2020-05-23 18:19:30 -0500

    Comit inicial con eclipse. Close #3

---
+++ AppCurso/src/main/java/co/com/springboot/AppCursoApplication.java
@@ -0,0 +1,13 @@
+package co.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCursoApplication.class, args);
+	}
+
+}
